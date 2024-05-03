7d375b84a6d82fd7dad1e996266cb00cc1c9317c
Kishoret1411 <kishoret1411@gmail.com>
2024-05-03 20:32:39 +0530

    feat : added register component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { RegisterLoginComponent } from './register-login/register-login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: RegisterLoginComponent},
+    {path: '', redirectTo: '/login', pathMatch:'full'}
+];
